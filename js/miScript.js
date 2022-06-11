2ea5caa61625a93b02ccb85aa931f2743f9a00ba
konny <co.pavez@duocuc.cl>
2022-06-11 02:59:45 -0400

    dzx

---
+++ js/miScript.js
@@ -0,0 +1,13 @@
+function validar(){
+    confirm("Desa Guardar los Cambios?")
+}
+
+
+function borrar(){
+    alert("Datos borrados")
+}
+
+function crear(){
+    let nom =document.getElementById("nom").value;
+    alert("Bienvenido " + nom);
+}
